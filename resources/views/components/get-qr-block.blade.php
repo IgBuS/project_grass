c2468b3a2087c478c8b3e973f6de2674d60e2103
Igor Buzin <i.b@msulab.ru>
2023-06-09 10:18:14 -0300

    First commit

---
+++ resources/views/components/get-qr-block.blade.php
@@ -0,0 +1,16 @@
+<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+<div class="static">
+<img class="rotate-90 sticky bottom-0 inline-flex" src="{{asset("images/andrew-forte-WJJZ4xv0Sz0-unsplash.png")}}">
+        <img class="sticky top-0 inline-flex" src="{{asset("images/andrew-forte-WJJZ4xv0Sz0-unsplash.png")}}">
+        <img class="inline-flex" src="{{asset("images/andrew-forte-WJJZ4xv0Sz0-unsplash.png")}}">
+    <div class="sticky top-0 flex-auto w-128 h-64 opacity-50 backdrop-blur bg-green-700 rounded-3xl">
+
+    </div>
+
+</div>
+
+
+
+</div>
+
+
